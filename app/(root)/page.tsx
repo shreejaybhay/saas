0d5a7c2299b6c63e139d44c31b6e9b66e614bd3d
Shree Jaybhay <shreejaybhay26@gmail.com>
2024-06-15 19:14:40 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>Home</div>
+      <div>
+        <p>hiii</p>
+      </div>
+    </div>
+  );
+}
